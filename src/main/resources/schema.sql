6ba26aa6405b557af0247bacefa9e8f57cc1d95b
Dmitrii <shamparov@gmail.com>
2018-10-11 02:10:03 +0300

    refactoring for linux

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,69 +1,69 @@
-DROP TABLE IF EXISTS BOOK_AUTHOR;
-DROP TABLE IF EXISTS BOOK_GENRE;
-DROP TABLE IF EXISTS BOOK_COMMENT;
-DROP TABLE IF EXISTS BOOK;
-DROP TABLE IF EXISTS GENRE;
-DROP TABLE IF EXISTS AUTHOR;
-DROP TABLE IF EXISTS COMMENT;
+DROP TABLE IF EXISTS book_author;
+DROP TABLE IF EXISTS book_genre;
+-- DROP TABLE IF EXISTS book_comment;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS author;
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS book;
 
-CREATE TABLE GENRE(
-  `ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `NAME` VARCHAR(255),
-  `DESCRIPTION` TEXT,
-  PRIMARY KEY (`ID`)
+CREATE TABLE genre(
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255),
+  `description` TEXT,
+  PRIMARY KEY (`id`)
 );
 
-CREATE TABLE AUTHOR(
-  `ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `NAME` VARCHAR(255),
-  PRIMARY KEY (`ID`)
+CREATE TABLE author(
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255),
+  PRIMARY KEY (`id`)
 );
 
-CREATE TABLE BOOK (
-  `ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `NAME` VARCHAR(255),
-  `DESCRIPTION` TEXT,
-  PRIMARY KEY (`ID`)
+CREATE TABLE book (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255),
+  `description` TEXT,
+  PRIMARY KEY (`id`)
 );
 
-CREATE TABLE COMMENT(
-  `ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `BOOK_ID` INT(11) NULL DEFAULT NULL,
-  `TEXT` TEXT,
-  PRIMARY KEY (`ID`)
+CREATE TABLE comment(
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `book_id` INT(11) NULL DEFAULT NULL,
+  `text` TEXT,
+  PRIMARY KEY (`id`),
+  INDEX `FK_comments_book` (`book_id`),
+  CONSTRAINT `FK_book_comment_book` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`)
 );
 
-CREATE TABLE `BOOK_AUTHOR` (
-  `ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `BOOK_ID` INT(11) NULL DEFAULT NULL,
-  `AUTHOR_ID` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  INDEX `FK_book_author_book` (`BOOK_ID`),
-  INDEX `FK_book_author_author` (`AUTHOR_ID`),
-  CONSTRAINT `FK_book_author_book` FOREIGN KEY (`BOOK_ID`) REFERENCES `BOOK` (`ID`),
-  CONSTRAINT `FK_book_author_author` FOREIGN KEY (`AUTHOR_ID`) REFERENCES `AUTHOR` (`ID`)
+CREATE TABLE `book_author` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `book_id` INT(11) NULL DEFAULT NULL,
+  `author_id` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK_book_author_book` (`book_id`),
+  INDEX `FK_book_author_author` (`author_id`),
+  CONSTRAINT `FK_book_author_book` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
+  CONSTRAINT `FK_book_author_author` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
 );
 
-CREATE TABLE `BOOK_GENRE` (
-  `ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `BOOK_ID` INT(11) NULL DEFAULT NULL,
-  `GENRE_ID` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  INDEX `FK_book_genre_book` (`BOOK_ID`),
-  INDEX `FK_book_genre_genre` (`GENRE_ID`),
-  CONSTRAINT `FK_book_genre_book` FOREIGN KEY (`BOOK_ID`) REFERENCES `BOOK` (`ID`),
-  CONSTRAINT `FK_book_genre_genre` FOREIGN KEY (`GENRE_ID`) REFERENCES `GENRE` (`ID`)
+CREATE TABLE `book_genre` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `book_id` INT(11) NULL DEFAULT NULL,
+  `genre_id` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK_book_genre_book` (`book_id`),
+  INDEX `FK_book_genre_genre` (`genre_id`),
+  CONSTRAINT `FK_book_genre_book` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
+  CONSTRAINT `FK_book_genre_genre` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`)
 );
 
-CREATE TABLE `BOOK_COMMENT` (
-  `ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `BOOK_ID` INT(11) NULL DEFAULT NULL,
-  `COMMENT_ID` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  INDEX `FK_book_comments_book` (`BOOK_ID`),
-  CONSTRAINT `FK_book_comment_book` FOREIGN KEY (`BOOK_ID`) REFERENCES `BOOK` (`ID`),
-  CONSTRAINT `FK_book_comment_comment` FOREIGN KEY (`COMMENT_ID`) REFERENCES `COMMENT` (`ID`)
-);
-
-
+-- CREATE TABLE `book_comment` (
+--   `id` INT(11) NOT NULL AUTO_INCREMENT,
+--   `book_id` INT(11) NULL DEFAULT NULL,
+--   `comment_id` INT(11) NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`),
+--   INDEX `FK_book_comments_book` (`book_id`),
+--   CONSTRAINT `FK_book_comment_book` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
+--   CONSTRAINT `FK_book_comment_comment` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`id`)
+-- );
 
