262fdb7d93d42d00ee221e7cbb6763621f92d565
Dmitry <shamparov@gmail.com>
2018-10-27 17:50:07 +0300

    author and genre tests

---
+++ src/test/java/ru/ezikvice/springotus/homework13/GenreEditorTests.java
@@ -0,0 +1,42 @@
+package ru.ezikvice.springotus.homework13;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringJUnit4ClassRunner.class)
+public class GenreEditorTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(roles = {"GENRE_EDITOR"})
+    public void
+    givenRoleGenreEditor_whenSaveGenre_thenOk() throws Exception {
+        this.mockMvc.perform(post("/genres/add")
+                .param("name", "test genre")
+                .param("description", "test description"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = {"GENRE_EDITOR"})
+    public void
+    givenRoleGenreEditor_whenSaveGenre_thenForbidden() throws Exception {
+        this.mockMvc.perform(post("/authors/add")
+                .param("name", "test author"))
+                .andExpect(status().isForbidden());
+    }
+
+}
