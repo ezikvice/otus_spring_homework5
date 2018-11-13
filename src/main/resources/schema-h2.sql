fc015ff7961a3bef3e9a683826e8409c99b8805b
Dmitry <shamparov@gmail.com>
2018-11-14 01:24:49 +0300

    fixed sql

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -9,22 +9,14 @@ CREATE TABLE book_author (
   id INT(11) NOT NULL AUTO_INCREMENT,
   book_id VARCHAR(25) NULL DEFAULT NULL,
   author_id VARCHAR(25) NULL DEFAULT NULL,
-  PRIMARY KEY (id),
-  INDEX FK_book_author_book (book_id),
-  INDEX FK_book_author_author (author_id),
-  CONSTRAINT FK_book_author_author FOREIGN KEY (author_id) REFERENCES author (id),
-  CONSTRAINT FK_book_author_book FOREIGN KEY (book_id) REFERENCES book (id)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE book_genre (
   id INT(11) NOT NULL AUTO_INCREMENT,
   book_id VARCHAR(25) NULL DEFAULT NULL,
   genre_id VARCHAR(25) NULL DEFAULT NULL,
-  PRIMARY KEY (id),
-  INDEX FK_book_genre_book (book_id),
-  INDEX FK_book_genre_genre (genre_id),
-  CONSTRAINT FK_book_genre_book FOREIGN KEY (book_id) REFERENCES book (id),
-  CONSTRAINT FK_book_genre_genre FOREIGN KEY (genre_id) REFERENCES genre (id)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE genre (
